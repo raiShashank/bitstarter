502ca46a87404bf971f722c302852463d32313c8
Shashank Rai <shashankrai02@gmail.com>
2013-07-10 09:07:10 +0000

    Reading data from index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+    var contents = fs.readFileSync('index.html');
+    var result = contents.toString();
+  response.send(result);
 });
 
 var port = process.env.PORT || 5000;
